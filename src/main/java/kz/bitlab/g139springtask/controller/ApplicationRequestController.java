60619246e444fc660afdd241774941cdacf11505
abylayquat22 <abylayquat22@gmail.com>
2025-10-04 18:52:34 +0500

    home html

--- src/main/java/kz/bitlab/g139springtask/controller/ApplicationRequestController.java
+++ src/main/java/kz/bitlab/g139springtask/controller/ApplicationRequestController.java
@@ -2,13 +2,20 @@ package kz.bitlab.g139springtask.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ApplicationRequestController {
 
     // TODO: пример
-    @GetMapping("/application-requests/list")
+    @GetMapping("/")
     public String list() {
+        return "home";
+    }
+
+    // TODO: пример
+    @PostMapping("/applications/add")
+    public String add() {
         return "home";
     }
 
