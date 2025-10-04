85d23f1c80bf9d69045ffa675e4b2a81e4a4767d
abylayquat22 <abylayquat22@gmail.com>
2025-10-04 18:32:48 +0500

    initial commit

---
+++ src/main/java/kz/bitlab/g139springtask/G139SpringTaskApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.g139springtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G139SpringTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G139SpringTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/bitlab/g139springtask/G139SpringTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.g139springtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G139SpringTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
