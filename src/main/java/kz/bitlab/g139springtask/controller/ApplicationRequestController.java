02e82f0a2a58a3781355e4c2d526cccca16afa32
abylayquat22 <abylayquat22@gmail.com>
2025-10-04 18:50:28 +0500

    exam[le endpoint

--- src/main/java/kz/bitlab/g139springtask/controller/ApplicationRequestController.java
+++ src/main/java/kz/bitlab/g139springtask/controller/ApplicationRequestController.java
@@ -1,7 +1,16 @@
 package kz.bitlab.g139springtask.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ApplicationRequestController {
+
+    // TODO: пример
+    @GetMapping("/application-requests/list")
+    public String list() {
+        return "home";
+    }
+
+
 }
