430d568abc253e7f8a1d181e2f74d0994df2d0ac
Nurlybek <nurlybek.soltanov@icloud.com>
2025-10-04 20:30:00 +0500

    Нурлыбек сделал получение всех ApplicationRequest юху

--- src/main/java/kz/bitlab/g139springtask/controller/ApplicationRequestController.java
+++ src/main/java/kz/bitlab/g139springtask/controller/ApplicationRequestController.java
@@ -1,15 +1,23 @@
 package kz.bitlab.g139springtask.controller;
 
+import kz.bitlab.g139springtask.model.ApplicationRequest;
+import kz.bitlab.g139springtask.service.ApplicationRequestService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ApplicationRequestController {
+
+    private final ApplicationRequestService applicationRequestService;
 
     // TODO: пример
     @GetMapping("/")
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("applications", applicationRequestService.applicationRequests());
         return "home";
     }
 
--- src/main/java/kz/bitlab/g139springtask/service/ApplicationRequestService.java
+++ src/main/java/kz/bitlab/g139springtask/service/ApplicationRequestService.java
@@ -1,7 +1,43 @@
 package kz.bitlab.g139springtask.service;
 
+import kz.bitlab.g139springtask.model.ApplicationRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ApplicationRequestService {
+
+    private final Connection connection;
+
+    public List<ApplicationRequest> applicationRequests() {
+        List<ApplicationRequest> applicationRequests = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "select * from application_requests"
+            );
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                ApplicationRequest applicationRequest = new ApplicationRequest();
+                applicationRequest.setId(resultSet.getLong("id"));
+                applicationRequest.setUserName(resultSet.getString("username"));
+                applicationRequest.setCourseName(resultSet.getString("courseName"));
+                applicationRequest.setCommentary(resultSet.getString("commentary"));
+                applicationRequest.setPhone(resultSet.getString("phone"));
+                applicationRequest.setHandled(resultSet.getBoolean("handled"));
+                applicationRequests.add(applicationRequest);
+            }
+            preparedStatement.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return applicationRequests;
+    }
 }
